82346563d67012d3cc5092d405b6a44a73d9c2ad
Arthurzineo1 <101883886+Arthurzineo1@users.noreply.github.com>
2024-09-19 20:05:21 -0300

    ISSUE #6 TESTE Update main.go
    TESTAR O FUNCIONAMENTO DO BUILDER

--- goagente/cmd/agent/main.go
+++ goagente/cmd/agent/main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"goagente/internal/data/hardware"
 	"goagente/internal/logging"
+	"goagente/internal/orchestration"
 	"log"
 )
 
 func main() {
-
+	// Inicializa o logger
 	loggerFactory, err := logging.NewLoggerFactory()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Certifique-se de fechar os loggers no final
 	defer loggerFactory.CloseLogger()
 
-	logging.Info("Executando DoWork em mypackage")
-	logging.Debug("Informações detalhadas sobre DoWork")
-	logging.Error(fmt.Errorf("erro durante a execução de DoWork"))
+	logging.Info("Iniciando a execução do HardwareInfoBuilder")
 
-	select {}
+	// Inicializa os retrievers de hardware (RAM, Discos, Processadores e Placa-mãe)
+	ramRetriever := hardware.WindowsRAMRetriever{}
+	diskRetriever := hardware.WindowsDiskRetriever{}
+	processorRetriever := hardware.WindowsProcessorRetriever{}
+	motherboardRetriever := hardware.WindowsMotherboardRetriever{}
+
+	// Inicializa o orquestrador com os retrievers
+	orchestrator := orchestration.NewHardwareOrchestrator(ramRetriever, diskRetriever, processorRetriever, motherboardRetriever)
+
+	// Executa o método Orchestrate para montar o HardwareInfo
+	patrimonio := "12345" // Exemplo de valor de patrimônio
+	hardwareInfo, err := orchestrator.Orchestrate(patrimonio)
+	if err != nil {
+		log.Fatal("Erro ao orquestrar informações de hardware:", err)
+	}
+
+	// Converte o objeto HardwareInfo em JSON
+	hardwareInfoJSON, err := json.MarshalIndent(hardwareInfo, "", "    ")
+	if err != nil {
+		log.Fatal("Erro ao converter HardwareInfo para JSON:", err)
+	}
+
+	// Exibe o JSON resultante
+	fmt.Println("Informações de Hardware em JSON:")
+	fmt.Println(string(hardwareInfoJSON))
 }
