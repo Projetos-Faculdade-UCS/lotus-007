1d129b4baee0b86b849c72fa9bbd36324309dbae
Arthurzineo1 <agoncalves3@ucs.br>
2024-09-22 18:38:49 -0300

    criacao dos objetos de looping

--- goagente/cmd/agent/main.go
+++ goagente/cmd/agent/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"goagente/internal/communication"
+	"goagente/internal/communication" // Importa o pacote do orquestrador de hardware
+	// Importa o pacote do orquestrador de programas
 	"goagente/internal/logging"
 	"goagente/internal/orchestration"
 	"log"
@@ -25,52 +25,45 @@ func main() {
 	patrimonio := "12345"
 	secretKey := "minhaChaveSecreta"
 
-	// Inicializa os mediadores com a chave secreta
-	hardwareMediator := orchestration.NewHardwareMediator(orchestration.NewHardwareOrchestrator(), poster, secretKey)
-	coreMediator := orchestration.NewCoreMediator(poster, secretKey)
-	programMediator := orchestration.NewProgramMediator(orchestration.NewProgramOrchestrator(), poster, secretKey)
+	// Inicializa o orquestrador de hardware e programas
+	hardwareOrchestrator := orchestration.NewHardwareOrchestrator()
+	programOrchestrator := orchestration.NewProgramOrchestrator()
 
-	// Executa as funções de orquestração em goroutines com looping de 5 segundos
-	go loopHardwareOrchestration(hardwareMediator, patrimonio)
-	go loopCoreOrchestration(coreMediator, patrimonio)
-	go loopProgramOrchestration(programMediator, patrimonio)
+	// Inicializa os mediadores com a chave secreta
+	hardwareMediator := orchestration.NewHardwareMediator(hardwareOrchestrator, poster, secretKey)
+	coreMediator := orchestration.NewCoreMediator(poster, secretKey)
+	programMediator := orchestration.NewProgramMediator(programOrchestrator, poster, secretKey)
+
+	// Cria os loops de orquestração com seus respectivos mediadores e intervalos
+	hardwareLoop := &orchestration.HardwareOrchestrationLoop{
+		OrchestrationLoop: orchestration.OrchestrationLoop{
+			Mediator:   hardwareMediator,
+			Patrimonio: patrimonio,
+			Interval:   60 * time.Second, // Define o intervalo de 5 segundos
+		},
+	}
+
+	coreLoop := &orchestration.CoreOrchestrationLoop{
+		OrchestrationLoop: orchestration.OrchestrationLoop{
+			Mediator:   coreMediator,
+			Patrimonio: patrimonio,
+			Interval:   60 * time.Second, // Define o intervalo de 5 segundos
+		},
+	}
+
+	programLoop := &orchestration.ProgramOrchestrationLoop{
+		OrchestrationLoop: orchestration.OrchestrationLoop{
+			Mediator:   programMediator,
+			Patrimonio: patrimonio,
+			Interval:   60 * time.Second, // Define o intervalo de 5 segundos
+		},
+	}
+
+	// Inicia os loops de orquestração em goroutines
+	go hardwareLoop.Start()
+	go coreLoop.Start()
+	go programLoop.Start()
 
 	// Mantém o programa em execução
 	select {}
-}
-
-// loopHardwareOrchestration faz a orquestração de hardware a cada 5 segundos
-func loopHardwareOrchestration(hardwareMediator *orchestration.HardwareMediator, patrimonio string) {
-	for {
-		err := hardwareMediator.OrchestrateAndPost(patrimonio)
-		if err != nil {
-			newErr := fmt.Errorf("erro ao orquestrar e enviar informações de hardware: %w", err)
-			logging.Error(newErr)
-		}
-		time.Sleep(5 * time.Second) // Pausa de 5 segundos
-	}
-}
-
-// loopCoreOrchestration faz a orquestração de core a cada 5 segundos
-func loopCoreOrchestration(coreMediator *orchestration.CoreMediator, patrimonio string) {
-	for {
-		err := coreMediator.OrchestrateAndPost(patrimonio)
-		if err != nil {
-			newErr := fmt.Errorf("erro ao orquestrar e enviar informações de core: %w", err)
-			logging.Error(newErr)
-		}
-		time.Sleep(5 * time.Second) // Pausa de 5 segundos
-	}
-}
-
-// loopProgramOrchestration faz a orquestração de programas a cada 5 segundos
-func loopProgramOrchestration(programMediator *orchestration.ProgramMediator, patrimonio string) {
-	for {
-		err := programMediator.OrchestrateAndPost(patrimonio)
-		if err != nil {
-			newErr := fmt.Errorf("erro ao orquestrar e enviar informações de programas: %w", err)
-			logging.Error(newErr)
-		}
-		time.Sleep(5 * time.Second) // Pausa de 5 segundos
-	}
 }
