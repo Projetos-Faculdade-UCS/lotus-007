165d85f71f10b863b9c6728394e9fc32776bfb9d
Arthur Gonçalves <78982690+Arthurzineo@users.noreply.github.com>
2024-10-02 18:40:27 -0300

    Update routes.go

--- goagente/internal/communication/routes.go
+++ goagente/internal/communication/routes.go
@@ -2,8 +2,8 @@ package communication
 
 // Definindo as rotas como strings
 const (
-	EnviaCoreInfos     = "v3/99a7790c-0a09-4dbc-a7f0-d426bc190ed2"
-	EnviaSystemInfos   = "v3/99a7790c-0a09-4dbc-a7f0-d426bc190ed2"
-	EnviaProgramInfos  = "v3/99a7790c-0a09-4dbc-a7f0-d426bc190ed2"
-	EnviaHardwareInfos = "v3/99a7790c-0a09-4dbc-a7f0-d426bc190ed2"
+	EnviaCoreInfos     = "aa"
+	EnviaSystemInfos   = "aa"
+	EnviaProgramInfos  = "aa"
+	EnviaHardwareInfos = "aa"
 )
